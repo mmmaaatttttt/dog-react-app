76011c2d242fcfc7648c43e56c54c71ae8936286
Matt Lane <lane.matthew@gmail.com>
2019-09-20 15:11:34 -0700

    cleaning up Dog component

--- src/Dog.js
+++ src/Dog.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import "./Dog.css";
 
 class Dog extends Component {
   render() {
-    return <div>Dog</div>
+    const { name, age, image, adoptable } = this.props.dog;
+    return (
+      <div className={adoptable ? "Dog Dog-adoptable" : "Dog"}>
+        <h3>{name}</h3>
+        <p>Age: {age} year(s)</p>
+        <img className="Dog-image" src={image} alt={`${name} profile pic`} />
+        <p>{name} is {adoptable ? "" : "not"} available for adoption!</p>
+      </div>
+    );
   }
 }
 
