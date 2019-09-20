c65743dd154f2fa80a3fef3a09ec02b19dd793b9
Matt Lane <lane.matthew@gmail.com>
2019-09-20 14:58:41 -0700

    more work on DogList, adding Dog too

---
+++ src/Dog.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Dog extends Component {
+  render() {
+    return <div>Dog</div>
+  }
+}
+
+export default Dog;
--- src/DogList.js
+++ src/DogList.js
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
+import Dog from "./Dog";
 
 class DogList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dogs: [
+        {
+          name: "Whiskey",
+          age: 6,
+          image:
+            "https://scontent-lax3-1.cdninstagram.com/vp/bf477169c5ffbd0527b55dbc86ff2ff3/5E3B24B1/t51.2885-15/e35/51188351_630582084045159_7876248144914958030_n.jpg?_nc_ht=scontent-lax3-1.cdninstagram.com&_nc_cat=103",
+          adoptable: false
+        }
+      ]
+    };
+  }
+
   render() {
-    return <div>DogList</div>
+    return (
+      <div>
+        Look at all of these dogs!!!
+        {this.state.dogs.map(dog => (
+          <Dog dog={dog} />
+        ))}
+      </div>
+    );
   }
 }
 
